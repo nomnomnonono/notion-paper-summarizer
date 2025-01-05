f525618976a4fb50889487a88d706312a65a0299
nomnomnonono <murumuru1204@gmail.com>
2025-01-05 14:09:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Notion Paper Summarizer
+## Overview
+Notion上の論文DBに自動で要約を追加するためのスクリプト。
+
+## Requirements
+- Python
+- Poetry
+
+## Installation
+1. 依存ライブラリのインストール
+```
+poetry install
+```
+
+2. `.env`にGemini APIキーを設定
+```
+GEMINI_API_KEY=gemini_api_key
+```
+
+## Usage
+```
+poetry run python src.main
+```
---
+++ src/main.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+from pydantic import BaseModel
+
+
+class Argments(BaseModel):
+    test: str
+
+    @classmethod
+    def parse_args(cls: BaseModel) -> BaseModel:
+        parser = ArgumentParser()
+        for k in cls.schema()["properties"]:
+            parser.add_argument(f"-{k[0:1]}", f"--{k}")
+        return cls.parse_obj(parser.parse_args().__dict__)
+
+
+def main(args: Argments) -> None:
+    return args
+
+
+if __name__ == "__main__":
+    args = Argments.parse_args()
+    main(args)
